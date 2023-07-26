f30f9b310752762e9c8b4ea17ecdc4bf16328663
Emanuel <emanuel_vera@live.com.ar>
2023-07-26 04:39:54 -0300

    Optimización de código: configuración CORS en módulo independiente, uso de variables de entorno y middlewares de Express en una sola línea

---
+++ api/src/corsConfig.js
@@ -0,0 +1,9 @@
+const allowedOrigin = process.env.ALLOWED_ORIGIN || 'http://localhost:3000';
+
+module.exports = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', allowedOrigin);
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+};
