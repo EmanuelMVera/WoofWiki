b04a02a9f55c1fa07d585d0bab91361d96dd50b2
EmanuelMVera <emanuel_vera@live.com.ar>
2023-09-13 04:47:02 -0300

    Formatee el modo en el que se muestran los temperamentos cuando se los llama de la base de datos para que tenga una estructura parecida a como se muestran cuando vienen de una api externa

--- api/src/services/breedDBService.js
+++ api/src/services/breedDBService.js
@@ -3,7 +3,26 @@ const { Breed, Temperament } = require("../db.js");
 // Función para obtener razas de la base de datos
 async function getBreedsFromDatabase() {
   try {
-    return await Breed.findAll({ include: Temperament });
+    const breeds = await Breed.findAll({
+      include: {
+        model: Temperament,
+        attributes: ["name"],
+      },
+    });
+
+    // Mapear y formatear los resultados
+    const formattedBreeds = breeds.map((breed) => ({
+      id: breed.id,
+      name: breed.name,
+      height: breed.height,
+      weight: breed.weight,
+      life_span: breed.life_span,
+      image: breed.image,
+      created: breed.created,
+      temperament: breed.Temperaments.map((temp) => temp.name),
+    }));
+
+    return formattedBreeds;
   } catch (error) {
     throw new Error("Error al obtener datos de la base de datos");
   }
